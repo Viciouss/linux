92f04eff7808751228e5929184d8fcb30b85ed23
Martin Jücker <martin.juecker@gmail.com>
2021-04-17 14:35:04 +0200

    usb: dwc2: add extcon support for dwc2
    add extcon support for dwc2 to switch usb roles
    
    Signed-off-by: Martin Jücker <martin.juecker@gmail.com>

--- drivers/usb/dwc2/core.h
+++ drivers/usb/dwc2/core.h
@@ -43,6 +43,7 @@
 #include <linux/usb/gadget.h>
 #include <linux/usb/otg.h>
 #include <linux/usb/phy.h>
+#include <linux/usb/role.h>
 #include "hw.h"
 
 /*
@@ -1057,6 +1058,10 @@ struct dwc2_hsotg {
 	struct dwc2_core_params params;
 	enum usb_otg_state op_state;
 	enum usb_dr_mode dr_mode;
+	struct extcon_dev *edev;
+	struct notifier_block edev_nb;
+	struct delayed_work edev_work;
+	enum usb_role new_role;
 	struct usb_role_switch *role_sw;
 	unsigned int hcd_enabled:1;
 	unsigned int gadget_enabled:1;
